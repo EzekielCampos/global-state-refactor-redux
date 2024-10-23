fa02cac47e0ca897c09f5dc26868275a754c3596
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-10-22 22:30:43 -0700

    adding the dispatch and state in components using redux hooks useDispatch and useSelector

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,10 @@ import {
 import { setContext } from '@apollo/client/link/context';
 
 import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
+// import { StoreProvider } from './utils/GlobalState';
+import {Provider} from "react-redux"
+import { store } from './utils/storeRedux';
+
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -32,10 +35,10 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <StoreProvider>
+      <Provider store={store}>
         <Nav />
         <Outlet />
-      </StoreProvider>
+      </Provider>
     </ApolloProvider>
   );
 }
